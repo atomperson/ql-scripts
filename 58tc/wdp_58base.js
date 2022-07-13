07c935499a8e4c6181f913578e1448bcf4ce7e4f
王大鹏 <1481433171@qq.com>
2022-07-13 09:45:55 +0800

    gjzz 脚本提交修改

--- 58tc/wdp_58base.js
+++ 58tc/wdp_58base.js
@@ -38,7 +38,6 @@ const ckkey = 'wbtcCookie';
 const notifyFlag = 1; //0为关闭通知，1为打开通知,默认为1
 const notify = $.isNode() ? require('./sendNotify') : '';
 let notifyStr = ''
-
 let httpResult //global buffer
 
 let userCookie = ($.isNode() ? process.env[ckkey] : $.getdata(ckkey)) || '';
