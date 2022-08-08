a3f1f015bdce5bc6bf1d96e11eb57805529f3d3e
王大鹏 <1481433171@qq.com>
2022-08-08 13:57:15 +0800

    时间提示库存

--- common/wdp_setinfo_dfxtl.js
+++ common/wdp_setinfo_dfxtl.js
@@ -464,12 +464,12 @@ async function selectHomePageData(token) {
 
     for (let i=0;i<haslist.length;i++) {
         addNotifyStr('商品名称:【'+haslist[i].title+'】',false);
-        addNotifyStr('总量:【'+haslist[i].totalStock+'】,库存:【'+haslist[i].stock+'】,积分:'+haslist[i].itemScore+'\n【skuId】:'+haslist[i].skuId,false);
+        addNotifyStr('总量:'+haslist[i].totalStock+',库存:【'+haslist[i].stock+'】,积分:'+haslist[i].itemScore+'\n【skuId】:'+haslist[i].skuId,false);
     }
     addNotifyStr('=====================【无库存商品】==========',false);
     for (let j=0;j<nohaslist.length;j++) {
         addNotifyStr('商品名称:【'+nohaslist[j].title+'】',false);
-        addNotifyStr('总量:【'+nohaslist[j].totalStock+'】,库存:【'+nohaslist[j].stock+'】,积分:'+nohaslist[j].itemScore+'',false);
+        addNotifyStr('总量:'+nohaslist[j].totalStock+',库存:【'+nohaslist[j].stock+'】,积分:'+nohaslist[j].itemScore+'',false);
 
     }
 }
